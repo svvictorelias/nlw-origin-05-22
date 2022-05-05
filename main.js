5c4be3d8e4892ce250fe185285e0f947cbe05a6c
Victor Elias <svvictorelias@gmail.com>
2022-05-05 17:19:47 -0300

    adicionado navBar completo abrindo e fechando

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ function onScroll(){
     navigation.classList.remove('scroll')
   }
 }
+
+function openMenu(){
+  document.body.classList.add('menu-expanded')
+}
+function closeMenu(){
+  document.body.classList.remove('menu-expanded')
+}
