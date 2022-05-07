eb78eee30aab8fe1c7c73c6424ec5675389110d2
Victor Elias <svvictorelias@gmail.com>
2022-05-07 15:49:32 -0300

    adicionado button para voltar ao inicio com função JS

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 function onScroll(){
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll(){
   if(scrollY>3){
     navigation.classList.add('scroll')
   }else{
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll(){
+  if(scrollY>500){
+    backToTopButton.classList.add('show')
+  }else{
+    backToTopButton.classList.remove('show')
   }
 }
 
